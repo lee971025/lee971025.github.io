b1ba661aea679a301117ceb0c5bf9e33b9237f53
lee971025 <54056058+lee971025@users.noreply.github.com>
2022-04-24 21:26:19 +0900

    Create greetings.js

---
+++ js/greetings.js
@@ -0,0 +1,43 @@
+const loginForm = document.getElementById("login-form");
+const loginInput = loginForm.querySelector("input");
+const loginButton = loginForm.querySelector("button");
+
+const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event){
+    event.preventDefault();
+    loginForm.classList.add("hidden");
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings();
+}
+
+function paintGreetings(){
+    const username = localStorage.getItem(USERNAME_KEY);
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+
+function handleLinkClick(event){
+    event.preventDefault();
+    console.log(event);
+    alert("clicked!");   
+}
+
+
+loginForm.addEventListener("submit", onLoginSubmit);
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+    //show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+}else{
+    //show the greetings
+   paintGreetings();
+}
