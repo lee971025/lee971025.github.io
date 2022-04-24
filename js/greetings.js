04779f745cfc8001771dcce74a61b8c8c1af4593
lee971025 <54056058+lee971025@users.noreply.github.com>
2022-04-25 00:50:53 +0900

    Update greetings.js

--- js/greetings.js
+++ js/greetings.js
@@ -17,7 +17,7 @@ function onLoginSubmit(event){
 
 function paintGreetings(){
     const username = localStorage.getItem(USERNAME_KEY);
-    greeting.innerText = `Hello ${username}`;
+    greeting.innerText = `HELLO! ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
