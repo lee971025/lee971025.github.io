0bba43d7707f8ebed25699b8f7b08c0fc49d494f
lee971025 <54056058+lee971025@users.noreply.github.com>
2022-04-24 21:25:08 +0900

    Create background.js

---
+++ js/background.js
@@ -0,0 +1,9 @@
+const images = ["1.jpeg", "2.jpeg", "3.jpeg"];
+
+const myImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+
+bgImage.src = `img/${myImage}`;
+
+document.body.appendChild(bgImage);
